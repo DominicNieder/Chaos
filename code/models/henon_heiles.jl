624e5cfe2f73030a0421c36cbac94994b99a5e38
DominiNieder <dominic.nieder@gmail.com>
2026-06-09 16:08:38 +0200

    working on code

--- code/models/henon_heiles.jl
+++ code/models/henon_heiles.jl
@@ -7,17 +7,27 @@ struct HenonHeiles
     E::Float64
 end
 
+
 function equations!(du, u, p, t)
+    m = 1
+    a = 1
+    w = 1
     x, y, px, py = u
     du[1] =  px
     du[2] =  py
-    du[3] = -x - 2x*y
-    du[4] = -y - x^2 + y^2
+    du[3] = -m*w^2*x - 2a*x*y
+    du[4] = -m*w^2*y - a*(x^2 + y^2)
 end
 
-energy(x, y, px, py) = 0.5(px^2 + py^2) + 0.5(x^2 + y^2) + x^2*y - y^3/3
+potential(x, y; m=1, w=1, a=1) = m*w^2/2 (x^2+y^2) + a*(x^2*y - y^3/3)
+
+kinetic(px, py; m=1, w=1, a=1) = (px^2 + py^2)/2
+
+hamiltonian(x, y, px, py; m=1, w=1, a=1) = kinetic(px, py, m=1, w=1, a=1) + potential(x,y, m=1, w=1, a=1)
 
 function solve_trajectory(u0; tspan=(0.0, 500.0), dt=0.01)
     prob = ODEProblem(equations!, u0, tspan)
     solve(prob, Vern9(), abstol=1e-10, reltol=1e-10, saveat=dt)
 end
+
+
