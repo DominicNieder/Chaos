e88e1bedc61ec59cd4b37f34c1c6ee43d74e4706
DominiNieder <dominic.nieder@gmail.com>
2026-06-17 14:53:31 +0200

    variable x or y_cut of surface section

--- code/analysis/section.jl
+++ code/analysis/section.jl
@@ -2,14 +2,14 @@ using OrdinaryDiffEq
 
 include("../models/henon_heiles.jl")
 
-# Collect section at y=0, py>0
-function surface_section_y0(sol)
+# Collect section at y=input, py>0
+function surface_section_y0(sol; y_cut=0)
     xs, pxs = Float64[], Float64[]
     for i in 2:length(sol.t)
         y_prev = sol[2, i-1]
         y_curr = sol[2, i]
-        if y_prev < 0 && y_curr >= 0
-            # linear interpolation to y=0 crossing
+        if y_prev < y_cut && y_curr >= y_cut
+            # linear interpolation to y=y_cut crossing
             α = -y_prev / (y_curr - y_prev)
             x  = sol[1, i-1] + α * (sol[1, i]  - sol[1, i-1])
             px = sol[3, i-1] + α * (sol[3, i]  - sol[3, i-1])
@@ -21,12 +21,12 @@ function surface_section_y0(sol)
 end
 
 # Collect section at x=0, px>0
-function surface_section_x0(sol)
+function surface_section_x0(sol; x_cut=0)
     ys, pys = Float64[], Float64[]
     for i in 2:length(sol.t)
         x_prev = sol[1, i-1]
         x_curr = sol[1, i]
-        if x_prev < 0 && x_curr >= 0
+        if x_prev < x_cut && x_curr >= x_cut
             # linear interpolation to x=0 crossing
             α = -x_prev / (x_curr - x_prev)
             y  = sol[2, i-1] + α * (sol[2, i]  - sol[2, i-1])
