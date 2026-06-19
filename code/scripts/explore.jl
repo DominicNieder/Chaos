7a99980103a5921f0574a094fceaf023f1648f03
DominiNieder <dominic.nieder@gmail.com>
2026-06-19 19:23:32 +0200

    save figures options

--- code/scripts/explore.jl
+++ code/scripts/explore.jl
@@ -4,17 +4,9 @@ include("../models/henon_heiles.jl")
 include("../analysis/section.jl")
 include("../styles/makie_theme.jl")
 
+mkpath(joinpath(@__DIR__, "../../figures/henon-heiles/explore"))
+
 # --- functions ---
-function save_axis(ax, traj_observables...; fname)
-    fig_out = Figure(size=(800, 700))
-    ax_out  = Axis(fig_out[1,1], title=ax.title[], xlabel=ax.xlabel[], ylabel=ax.ylabel[])
-    for obs in traj_observables
-        obs(ax_out)   # caller provides a lambda that re-plots
-    end
-    mkpath(dirname(fname))
-    save(fname, fig_out, px_per_unit=2)
-    @info "Saved $fname"
-end
 
 
 # --- simulation variables ---
@@ -25,7 +17,6 @@ py0= Observable(Float64(init_var[4]))
 T  = Observable(  Int64( num_int[1]))
 
 # --- analysis observables ---
-
 y_sec_obs  = Observable(Float64[]) # surface of section
 py_sec_obs = Observable(Float64[])
 
@@ -34,6 +25,12 @@ py0_range_obs = Observable(Float64[])
 
 # --- figure parameters---
 ideal_size= (1920 , 1200)
+
+# --- contour plot ---
+r      = range(-1.0, 1.0, length=120)
+levels = logrange(5.0*0.009, 6.9*0.089, 7)
+epot   = [potential(x,y; p=param) for x in r, y in r]
+
 
 with_theme(QUARTO_THEME) do
     init = lift(E, x0, y0, py0, T) do e, x, y, py, T
@@ -64,66 +61,164 @@ with_theme(QUARTO_THEME) do
         s
     end
 
+    param_label = lift(E, init) do e, (x, y, px, py, T)
+        a, m, w = param
+        """
+        a  = $a
+        m  = $m
+        ω  = $w
+        T  = $T
+        dt = $(num_int[2])
+        E  = $e
+        x₀ = $x
+        y₀ = $y
+        py₀= $py
+        px₀= $px
+        """
+    end
+
+
+
     # --- (phase space) trajectory ---
     traj_x = lift(s -> s[1, :], sol)   # all x values: row 1, all timesteps
     traj_y = lift(s -> s[2, :], sol)   # all y values: row 2, all timesteps
 
 
-    # --- layout ---
+    # === layout ===
     fig = Figure(size=ideal_size)
-    # real space trajectory
-    ax1 = Axis(fig[1:2,1], title="Space (x, y)", xlabel="x", ylabel="y")
+
+    # == Plot: configuration space ==
+    ax1 = Axis(fig[1:2,1], title="Configuration space", xlabel="x", ylabel="y")
     xlims!(ax1, -1.0, 1.0)
-    ylims!(ax1, -1.0, 1.0)
+    ylims!(ax1, -0.6, 1.2)
     lines!(ax1, traj_x, traj_y, linewidth=1.0)
-    # plotting the contour of potential into fig 1
-    r = range(-1.0, 1.0, length = 120)
-    levels = logrange(5.0*0.009,6.9*0.089,7)
-    epot = [potential(x,y; p=param) for x in r, y in r]
+
     contour!(ax1, r, r, epot, labels=true, levels=levels, colormap=:hsv, colorscale=identity)
 
-    # phase space trajectory
-    ax2 = Axis(fig[1:2,2], title="Section (x=0)", xlabel="y", ylabel="p_y")
-    scatter!(ax2, y_sec_obs, py_sec_obs, markersize=5)
-    # calculating maximum cricumfarence of phase space trajectory
+    Label(fig[2, 3][1,1:4], "Save options")
+    # --- save trajectory figure ---
+    save_fig_btn_traj = Button(fig[2, 3][2,1], label="traj", labelcolor=:black)
+    on(save_fig_btn_traj.clicks) do _
+        println("--- Saving trajectory fiugre---")
+        fname = joinpath(@__DIR__, "../../figures/henon-heiles/explore", "traj-$(round(E[], digits=4))-$(x0[])-$(y0[])-$(py0[]).png")
+        # save only ax1 (trajectory plot)
+        fig_export = Figure(size=ideal_size)
+        ax_export = Axis(fig_export[1,1])
+        lines!(ax_export, traj_x[], traj_y[], linewidth=1.0)
+        contour!(ax_export, r, r, epot, labels=true, levels=levels, colormap=:hsv, colorscale=identity)
+        xlims!(ax_export,  -1, 1)
+        ylims!(ax_export, -0.6, 1)
+        ax_export.xlabel = "x"
+        ax_export.ylabel = "y"
+        save(fname, fig_export, px_per_unit=2)
+        println("--- done ---")
+    end
 
+    # == Plot: phase space trajectory ==
+    ax2 = Axis(fig[1:2,2], title="Section (x=0)", xlabel="y", ylabel="p_y")
+    scatter!(ax2, y_sec_obs, py_sec_obs, markersize=5)  # from trajectory
+    # calculating maximum cricumfarence of phase space trajectory
     on(E; update=true) do e
         y0_roots        = real.(filter(r -> abs(imag(r)) < 1e-10, limit_of_initial_y0(e)))
-        y0_1, y0_2 = extrema(y0_roots)  # y0_roots[1], y0_roots[2]
-        y0_range_obs[]  = collect(range(y0_1, y0_2, length=100))
-        py0_range_obs[] = limit_of_initial_py0(y0_range_obs[], e)
+        y0_1, y0_2 = y0_roots[1], y0_roots[2]
+        dy = (y0_roots[2] - y0_roots[1])/100
+        y0_range       = collect(range(y0_1, y0_2, length=100))
+        y0_range_obs[] = y0_range
+        py0_range_obs[] = limit_of_initial_py0(y0_range, e)
         _, p_max    = extrema(py0_range_obs[])
-        xlims!(ax2, y0_1,y0_2)
-        ylims!(ax2, -p_max, p_max)
+        dp = p_max/50
+        xlims!(ax2, y0_1-dy, y0_2+dy)
+        ylims!(ax2, -p_max-dp, p_max+dp)
     end
-    scatter!(ax2, y0_range_obs, py0_range_obs, markersize=5, color=C_CREAM)
-    scatter!(ax2, y0_range_obs, lift(v -> -v, py0_range_obs), markersize=5, color=C_CREAM)
+    scatter!(ax2, y0_range_obs, py0_range_obs, markersize=5, color=C_CREAM)  # max py
+    scatter!(ax2, y0_range_obs, lift(v -> -v, py0_range_obs), markersize=5, color=C_CREAM) # min -py
 
 
-    # x(t)
+    # --- save surface section figure ---
+    save_fig_btn_phasesect = Button(fig[2, 3][2,2], label="section", labelcolor=:black)
+    on(save_fig_btn_phasesect.clicks) do _
+        println("--- save surface sectio figure ---")
+
+        fname = joinpath(@__DIR__, "../../figures/henon-heiles/explore", "surf_of_sec-$(round(E[], digits=4))-$(x0[])-$(y0[])-$(py0[]).png")
+        # save only ax1 (trajectory plot)
+        fig_export = Figure(size=ideal_size)
+        ax_export = Axis(fig_export[1,1])
+        lines!(ax_export, y_sec_obs[], py_sec_obs[], linewidth=1.0)
+        scatter!(ax_export, y0_range_obs[], py0_range_obs[], markersize=5, color=C_CREAM)  # max py
+        scatter!(ax_export, y0_range_obs[], -py0_range_obs[], markersize=5, color=C_CREAM) # min -py
+        y_min, y_max =  extrema(y0_range_obs[])
+        p_max        =  maximum(py0_range_obs[])
+        dy           =  (y_max-y_min)/50
+        dp           =  p_max/30
+        ylims!(ax_export,  -p_max-dp, p_max+dp)
+        xlims!(ax_export, y_min-dy, y_max+dy)
+        ax_export.xlabel = "y"
+        ax_export.ylabel = "p_y"
+        save(fname, fig_export, px_per_unit=2)
+        println("--- done ---")
+
+    end
+
+    # == Plot: x(t) ==
     sol_t = lift(s->s.t, sol)
     ax3 = Axis(fig[3,1:2], title="x(t)", xlabel="t", ylabel="x")
     lines!(ax3, sol_t, traj_x, linewidth=0.5)
     ylims!(ax3,  -1.0, 1.0)
-    on(T; update=true) do t
-        xlims!(ax3, 0.0, t)
+
+
+    # --- save x(t) figure ---
+    save_fig_btn_xt = Button(fig[2, 3][2,3], label="x(t)", labelcolor=:black)
+    on(save_fig_btn_xt.clicks) do _
+        println("--- save x(t) ---")
+        fname = joinpath(@__DIR__, "../../figures/henon-heiles/explore", "xt-$(round(E[], digits=4))-$(x0[])-$(y0[])-$(py0[]).png")
+        # save only ax1 (trajectory plot)
+        fig_export = Figure(size=ideal_size)
+        ax_export = Axis(fig_export[1,1])
+        lines!(ax_export, sol_t[], traj_x[], linewidth=0.5)
+        ylims!(ax_export,  -1.0, 1.0)
+        xlims!(ax_export, 0.0, T[]+0.01)
+        ax_export.xlabel = "t"
+        ax_export.ylabel = "x(t)"
+        save(fname, fig_export, px_per_unit=2)
+        println("--- done ---")
+
     end
 
-    # y(t)
+    # == Plot: y(t) ==
     ax4 = Axis(fig[4,1:2], title="y(t)", xlabel="t", ylabel="y")
     lines!(ax4, sol_t, traj_y, linewidth=0.5)
     ylims!(ax4,  -1.0, 1.0)
+    # update the time axis of trajectories
     on(T; update=true) do t
-        xlims!(ax4, 0.0, t)
+        xlims!(ax3, 0.0, t+0.01)
+        xlims!(ax4, 0.0, t+0.01)
     end
 
-    # --- controls ---
+
+    # --- save y(t) figure ---
+    save_fig_btn_yt = Button(fig[2, 3][2,4], label="y(t)", labelcolor=:black)
+    on(save_fig_btn_yt.clicks) do _
+        println("--- save y(t) ---")
+        fname = joinpath(@__DIR__, "../../figures/henon-heiles/explore", "yt-$(round(E[], digits=4))-$(x0[])-$(y0[])-$(py0[]).png")
+        # save only ax1 (trajectory plot)
+        fig_export = Figure(size=ideal_size)
+        ax_export = Axis(fig_export[1,1])
+        lines!(ax_export, sol_t[], traj_y[], linewidth=0.5)
+        ylims!(ax_export,  -1.0, 1.0)
+        xlims!(ax_export, 0.0, T[]+0.01)
+        ax_export.xlabel = "t"
+        ax_export.ylabel = "y(t)"
+        save(fname, fig_export, px_per_unit=2)
+        println("--- done ---")
+    end
+
+    # === controls of initial parameters ===
     sg = SliderGrid(fig[1, 3],
-        (label="Energy E",  range=0.01:0.0001:0.3,   startvalue=0.083),
+        (label="Energy E",  range=0.01:0.0001:0.16667,   startvalue=0.083),
         (label="x₀",        range=-1.0:0.01:1.0,    startvalue=0.0),
         (label="y₀",        range=-0.5:0.01:1.0,    startvalue=0.0),
         (label="py₀",       range=0.0:0.01:1.0,     startvalue=0.0),
-        (label="T",       range=100:50:2000,     startvalue=num_int[1])
+        (label="T",       range=600:200:10000,     startvalue=num_int[1])
     )
     on(sg.sliders[1].value) do v; E[]   = v; end
     on(sg.sliders[2].value) do v; x0[]  = v; end
@@ -131,27 +226,13 @@ with_theme(QUARTO_THEME) do
     on(sg.sliders[4].value) do v; py0[] = v; end
     on(sg.sliders[5].value) do v; T[]   = v; end
 
-    # --- save figure/data ---
-    # Label(fig[2, 3][1,1:4], "save figure")
-    # save_fig_btn_traj = Button(fig[2, 3][1,1], label="traj")
-    # save_fig_btn_phasesect = Button(fig[2, 3][1,2], label="section")
-    # save_fig_btn_xt = Button(fig[2, 3][1,3], label="x(t)")
-    # save_fig_btn_yt = Button(fig[2, 3][1,4], label="y(t)")
+    # displaying all parameters
+    Label(fig[3, 3], param_label, justification=:left, font="JetBrainsMono Nerd Font")
+    is_valid = lift(init) do (x, y, px, py, T)
+        px == 0.0 && py == 0.0 ? "⚠ outside energy surface" : ""
+    end
+    Label(fig[5, 1:2], is_valid, color=:red)
 
-    # on(save_fig_btn.clicks) do _
-    #     fname = joinpath(@__DIR__, "../../figures/henon-heiles", "explore_E$(round(E[], digits=4)).png")
-    #     mkpath(dirname(fname))
-    #     # save only ax1 (trajectory plot)
-    #     fig_export = Figure(size=ideal_size)
-    #     ax_export = Axis(fig_export[1,1])
-    #     lines!(ax_export, traj_x[], traj_y[], linewidth=1.0)
-    #     save(fname, fig_export, px_per_unit=2)
-    # end
-
-
-
-
-    Label(fig[5, 1:2], lift(v -> "effective py₀ = $(round(v[4], digits=4))", init))
 
     display(GLMakie.Screen(), fig)
 end
