2c46b96840bcbaa5a7140fb7cbe6adcb25334a18
DominiNieder <dominic.nieder@gmail.com>
2026-06-19 15:58:55 +0200

    surf of sec: enclosure and efficiancy

--- code/models/henon_heiles.jl
+++ code/models/henon_heiles.jl
@@ -1,6 +1,6 @@
 using OrdinaryDiffEq
 using JSON3  # or JSON
-
+using Polynomials
 # ---------------------------------------------------
 #               Simulation Settings
 # ---------------------------------------------------
@@ -39,23 +39,74 @@ end
 
 
 # Energies, param= (a, m, w)
-potential(x, y; p=param) = p[2]*p[3]^2/2 *(x^2+y^2) + p[1]*(x^2*y - y^3/3)
+potential(x::Real, y::Real; p=param) = p[2]*p[3]^2/2 *(x^2+y^2) + p[1]*(x^2*y - y^3/3)
 
 kinetic(px, py; m=param[2]) = (px^2 + py^2)/(2*m)
 
 hamiltonian(x, y, px, py; p=param) = kinetic(px, py, m=p[2]) + potential(x,y, p=p)
 
 
-# numerical integration, num_int = (T, dt)
 """
+makes a function that filters the surface of section values whilst solving the ODE
+"""
+function section_callback(section_q, section_p)
+    condition(u, t, integrator) = u[1]
+    affect!(integrator) = begin
+        if integrator.u[3] >= 0
+            push!(section_q, integrator.u[2])
+            push!(section_p, integrator.u[4])
+        end
+    end
+    ContinuousCallback(condition, affect!)
+end
+
+"""
+u0 -> initial conditions
+tspan   =   (tmin, tmax)
+dt      =   timestep size
+p       =   parameters of the henon heiles modle
+
 Uses Vern9 algorithem: 
 "Verner's “Most Efficient” 9/8 Runge-Kutta method. (lazy 9th order interpolant)"
 """
-function solve_trajectory(u0; 
-    tspan=(0.0, num_int[1]), dt=num_int[2], p=param
+function solve_trajectory(
+    u0; 
+    tspan=(0.0, num_int[1]), 
+    dt=num_int[2], 
+    p=param, 
+    callback=nothing
     )
-    prob = ODEProblem(equations!, u0, tspan, p)
-    solve(prob, Vern9(), abstol=1e-10, reltol=1e-10, saveat=dt)
+    prob = ODEProblem(
+        equations!, u0, tspan, p
+        )
+    solve(
+        prob, 
+        Vern9(), 
+        abstol=1e-10, 
+        reltol=1e-10, 
+        saveat=dt, 
+        callback=callback
+        )
 end
 
 
+"""
+calculates the limit for y0, for surface section x=0.
+    Beware that the expression is set at
+    alpha   =   1
+    m       =   1
+    w       =   1
+"""
+function limit_of_initial_y0(E::Float64)
+    p=Polynomial([2E, 0,-1, 2/3])
+    print(p)
+    roots(p)
+end
+
+function limit_of_initial_py0(y::Real, E::Float64)
+   sqrt(max(0.0, 2*(E - potential(0.0, y))))
+end
+
+function limit_of_initial_py0(y::AbstractVector{<:Real}, E::Float64)
+    map(yi -> sqrt( max(0.0, 2*(E - potential(0.0, yi) )) ), y)
+end
