c8167a4ff58aa1f58784a46931c6997f9debccef
DominiNieder <dominic.nieder@gmail.com>
2026-06-30 17:17:39 +0200

    here I want to select particular figures

--- code/analysis/Henon-Heiles-simResults.jl
+++ code/analysis/Henon-Heiles-simResults.jl
@@ -5,9 +5,23 @@ include("../styles/makie_theme.jl")
 include("../models/henon_heiles.jl")
 using .HenonHeiles
 
-DATA_DIR=joinpath(@__DIR__,"../../data/henon-heiles/simulation")
+DATA_DIR        = joinpath(@__DIR__, "../../data/henon-heiles/simulation")
+FIG_DIR         = joinpath(@__DIR__, "../../figures/henon-heiles/simulation")
+
+FIG_ORIENTATION  = joinpath(FIG_DIR, "orientation.json")
+DATA_ORIENTATION = joinpath(DATA_DIR, "orientation.json")
+
 EXPL_DATA_DIR = joinpath(@__DIR__, "../../data/henon-heiles/explore")
-FIG_DIR = joinpath(@__DIR__, "../../figures/henon-heiles/simulation")
+
+json = JSON3.read(read(DATA_ORIENTATION, String))
+
+run = json[:"(E,y)100x256_p00.0/"]
+file = map(i -> String(i), run.file)
+
+
+println(typeof(file))
+energies = map(i -> Float64(i),run.energy)
+println(typeof(energies))
 
 println("data directory: $(isdir(DATA_DIR)),\nexplore directory: $(isdir(EXPL_DATA_DIR)),\nfigures directory: $(isdir(FIG_DIR))")
 
@@ -17,7 +31,7 @@ fsize=(1200,1200)
 e = 0.1243
 param = (1,1,1)
 n=200
-function section_set_plot_lim(e,param, n)
+function section_set_plot_lim(e,param,n)
     sec_boundry = HenonHeiles.section_boundary_ranges(e, param, n)
     y_max, py_max = sec_boundry[1], sec_boundry[2]
     boundary = HenonHeiles.section_boundary(y_max, py_max)
@@ -33,32 +47,34 @@ end
 
 
 file1 = joinpath(DATA_DIR, "E0.1243-T10000.0-py0.0-n128.jld2")
-
 data  = load(file1)
 results = data["results"]
 resoltuion = length(data["results"])
 println(typeof(results), keys(results[1]))
 resolution = 128
-b1, ylim1, pylim1 = section_set_plot_lim(e,param,n)
 fname   = "E$(round(e,digits=4))-T$(10000)-py0.0-n128.png"
 outfig = joinpath(FIG_DIR, fname)
 
-with_theme(QUARTO_THEME) do
-    fig1 = Figure(size=(1200,1200))
-    ax  = Axis(fig1[1,1], xlabel="y", ylabel="p_y")
-    scatter!(ax, b1)
+function plot_phasespaceof!(ax, e, param, data, fig_size)
+    b1, ylim1, pylim1 = HenonHeiles.section_set_plot_lim(e,param,120)
     colors = cgrad(:viridis, resolution)[range(0, 1, length=resolution)]
     for i in eachindex(results)
         scatter!(ax, results[i][:sec_y], results[i][:sec_py], color=colors[i], markersize=3)
     end
     ylims!(ax, pylim1...)
     xlims!(ax, ylim1...)
+
+end
+
+with_theme(QUARTO_THEME) do
+    fig1 = Figure(size=fig_size)
+    ax  = Axis(fig1[1,1], xlabel="y", ylabel="p_y")
+    scatter!(ax, b1)
+    plot_phasespaceof!(ax, e, param, data, fig_size=(1200,1200))    
     # --- save figure ---
     save_screen = GLMakie.Screen(; visible=false)
     display(save_screen, fig1)
     save(outfig, fig1, px_per_unit=2)
     close(save_screen)
-
     display(GLMakie.Screen(), fig1)
 end
-
